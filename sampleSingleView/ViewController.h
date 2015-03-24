ffb1da93946f6c95a4480cdd38c4e6b323e5aaef
user25857 <user25857@74-80-237-104.ic2net.net>
2015-03-24 08:39:12 -0700

    Initial Commit

---
+++ sampleSingleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sampleSingleView
+//
+//  Created by user25857 on 3/24/15.
+//  Copyright (c) 2015 kritika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sampleSingleView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sampleSingleView
+//
+//  Created by user25857 on 3/24/15.
+//  Copyright (c) 2015 kritika. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
